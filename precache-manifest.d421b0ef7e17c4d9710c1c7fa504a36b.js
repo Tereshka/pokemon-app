0884eb360e5bbd1e4d374a8f5731fd44f8573036
Tereshka <tereshka07@gmail.com>
2020-03-31 21:19:03 +0900

    Updates

---
+++ precache-manifest.d421b0ef7e17c4d9710c1c7fa504a36b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edbc4d63d883b7950c32a69efb69cec6",
+    "url": "/pokemon-app/index.html"
+  },
+  {
+    "revision": "07f10e96ee5c8b244f64",
+    "url": "/pokemon-app/static/css/main.028a9090.chunk.css"
+  },
+  {
+    "revision": "1feb5a5ed08992989d98",
+    "url": "/pokemon-app/static/js/2.fd118bb1.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/pokemon-app/static/js/2.fd118bb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07f10e96ee5c8b244f64",
+    "url": "/pokemon-app/static/js/main.d419fe0e.chunk.js"
+  },
+  {
+    "revision": "b6f0c0491a9b1e77f46e",
+    "url": "/pokemon-app/static/js/runtime-main.925a2edf.js"
+  }
+]);
